00112104fcdfe6e5b0ce44361419309a08d9027c
Sebastien Galina <sebastien@boldly.ca>
2015-07-26 17:35:42 -0700

    added new banner photo, photos to about page, and photos to burnaby detail page

--- portfolio/static/js/dropdown.js
+++ portfolio/static/js/dropdown.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
 	$("#menu .dropdown-toggle").click(function() {
 		$("#menu .button-grid .grid").toggleClass("active");
 	});
+
+	$("#wrap").click(function() {
+		$("#menu .button-grid .grid").removeClass("active");
+	});
 });

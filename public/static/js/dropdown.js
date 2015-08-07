ab8f291d14c9601285b21e0a2f66f88deacb009a
Sebastien Galina <sebastien@boldly.ca>
2015-08-07 14:52:54 -0700

    cleaned up navigation bar a little

--- public/static/js/dropdown.js
+++ public/static/js/dropdown.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	$("#menu .dropdown-toggle").click(function() {
 		$("#menu .button-grid .grid").toggleClass("active");
+		$("main").toggleClass("dropdown");
 	});
 
 	$("#wrap, #footer-main, #menu .button-grid .btn").click(function() {
 		$("#menu .button-grid .grid").removeClass("active");
+		$("main").removeClass("dropdown");
 	});
 });
